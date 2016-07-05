921ed10da9cd2ceee4fca47aa92c37cd7f31e2f1
Larry Lauer <larry.lauer.3d@gmail.com>
2016-07-05 00:54:35 -0700

    Added basics chapter exercises 1-6

---
+++ basics_ex3.rb
@@ -0,0 +1,13 @@
+# exercise 3 of basiscs #
+
+# store a hash of movies and release dates #
+movies = { the_matrix: 1999, hackers: 1995, star_wars: 1977, blade_runner: 1982, sunshine: 2007 }
+
+puts "Larry's movie list:"
+
+# revisit when you figure out how to pull the hash name #
+puts "The Matrix", + movies [:the_matrix]
+puts "Hackers", + movies [:hackers]
+puts "Star Wars", + movies [:star_wars]
+puts "Blade Runner", + movies [:blade_runner]
+puts "Sunshine", + movies [:sunshine]
