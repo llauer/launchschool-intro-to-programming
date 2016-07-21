5160b45b50f79154406e4729cd5274e34566024c
Larry Lauer <larry.lauer.3d@gmail.com>
2016-07-21 00:31:33 -0700

    working on loops

---
+++ loops/exercise1.rb
@@ -0,0 +1,7 @@
+# exercise1.rb
+
+x = [1, 2, 3, 4, 5]
+
+x.each do |a|
+  a + 1
+end
---
+++ loops/exercise2.rb
@@ -0,0 +1,9 @@
+# exercise2.rb
+
+x = ""
+while x != "STOP" do
+  puts "Hi, How are you feeling?"
+  ans = gets.chomp
+  puts "Want me to ask you again?"
+  x = gets.chomp.upcase
+end
